9e43ec38bbb4cc3bbbf7ab96be978b731ada77d0
bigben-dev777 <albertojohn20041103@gmail.com>
2025-12-17 11:52:24 -0500

    change the commit

--- src/components/ContactCard/index.tsx
+++ src/components/ContactCard/index.tsx
@@ -14,78 +14,78 @@ const initData = [
     desc: "Boulevard 2442 Caava, California",
   },
   {
-    title: "Contact us now",
+    title: "Contact us hsadfjkhdlfasjldf",
     button: "SEND US A MESSAGE",
   },
 ];
 
-const ContactCard = () => {
-  return (
-    <Paper
-      sx={(theme) => ({
-        [theme.breakpoints.down("md")]: {
-          background: "transparent",
-        },
-      })}
-    >
-      <Stack
-        flex={1}
-        sx={{
-          flexDirection: "column",
-          gap: "24px",
-          padding: "24px",
-          justifyContent: "center",
-        }}
-      >
-        <Stack>
-          <Typography
-            variant="h3"
-            sx={{
-              fontFamily: "Inter",
-            }}
-          >
-            Contact Us
-          </Typography>
-          <Typography variant="h6" color="textSecondary">
-            contact us for some help
-          </Typography>
-        </Stack>
-        <Stack
-          sx={{
-            gap: "16px",
-          }}
-        >
-          {initData.map((item) => (
-            <Stack
-              sx={{
-                flexDirection: "row",
-              }}
-            >
-              <Typography
-                flex={1}
-                variant="h5"
-                sx={{
-                  fontWeight: "600",
-                }}
-              >
-                {item.title}
-              </Typography>
-              {item.desc !== undefined && (
-                <Typography variant="h6" color="textSecondary">
-                  {item.desc}
-                </Typography>
-              )}
-              {item.button !== undefined && (
-                <Button variant="contained" color="primary">
-                  {item.button}
-                </Button>
-              )}
-            </Stack>
-          ))}
-        </Stack>
-      </Stack>
-    </Paper>
-  );
-};
+// const ContactCard = () => {
+//   return (
+//     <Paper
+//       sx={(theme) => ({
+//         [theme.breakpoints.down("md")]: {
+//           background: "transparent",
+//         },
+//       })}
+//     >
+//       <Stack
+//         flex={1}
+//         sx={{
+//           flexDirection: "column",
+//           gap: "24px",
+//           padding: "24px",
+//           justifyContent: "center",
+//         }}
+//       >
+//         <Stack>
+//           <Typography
+//             variant="h3"
+//             sx={{
+//               fontFamily: "Inter",
+//             }}
+//           >
+//             Contact Us
+//           </Typography>
+//           <Typography variant="h6" color="textSecondary">
+//             contact us for some help
+//           </Typography>
+//         </Stack>
+//         <Stack
+//           sx={{
+//             gap: "16px",
+//           }}
+//         >
+//           {initData.map((item) => (
+//             <Stack
+//               sx={{
+//                 flexDirection: "row",
+//               }}
+//             >
+//               <Typography
+//                 flex={1}
+//                 variant="h5"
+//                 sx={{
+//                   fontWeight: "600",
+//                 }}
+//               >
+//                 {item.title}
+//               </Typography>
+//               {item.desc !== undefined && (
+//                 <Typography variant="h6" color="textSecondary">
+//                   {item.desc}
+//                 </Typography>
+//               )}
+//               {item.button !== undefined && (
+//                 <Button variant="contained" color="primary">
+//                   {item.button}
+//                 </Button>
+//               )}
+//             </Stack>
+//           ))}
+//         </Stack>
+//       </Stack>
+//     </Paper>
+//   );
+// };
 
 export default ContactCard;
